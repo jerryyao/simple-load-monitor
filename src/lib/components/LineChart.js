5c59a057dfaaa42ed7cc596a173d704e77eabf87
Matteo Palvarini <teo.palvarini@gmail.com>
2017-03-25 10:34:33 -0500

    Revert "Remove react lifecycle hooks"
    This reverts commit 67bfe20c35e4cc1422c6c059a034ace750faaa0f.

--- src/lib/components/LineChart.js
+++ src/lib/components/LineChart.js
@@ -3,13 +3,16 @@ import * as d3 from 'd3';
 import classes from './lineChart.css';
 
 class LineChart extends React.Component {
-  _renderLine = () => {
-    const { data, width, height } = this.props;
+  state = {
+    x: null,
+    y: null,
+  }
 
+  componentWillReceiveProps(nextProps) {
+    const { data, width, height } = nextProps;
     data.forEach((d) => {
       d.date = new Date(d.timestamp);
     });
-
     const x = d3.scaleTime()
       .domain(d3.extent(data, d => d.date))
       .range([0, width]);
@@ -17,6 +20,57 @@ class LineChart extends React.Component {
     const y = d3.scaleLinear()
       .domain([0, d3.max(data, d => d.loadavg)])
       .range([height, 0]);
+
+    this.setState({ x, y });
+  }
+
+  componentDidMount() {
+    const { x, y } = this.state;
+
+    // Add the X Axis
+    const xAxis = d3.axisBottom(x).tickArguments(5);
+    d3.select(this.svg).append('g')
+      .attr('class', 'xAxis')
+      .attr('transform', `translate(0,${this.props.height})`)
+      .call(xAxis);
+
+    // Add the Y Axis
+    const yAxis = d3.axisLeft(y).tickArguments(5);
+    d3.select(this.svg).append('g')
+      .attr('class', 'yAxis')
+      .call(yAxis)
+      .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Load Average");
+  }
+
+  componentDidUpdate() {
+    const { x, y } = this.state;
+
+    // // Add the X Axis
+    // const xAxis = d3.axisBottom(x).tickArguments(5);
+    // d3.select(this.svg).append('g')
+    //   .attr('class', 'x axis')
+    //   .attr('transform', `translate(0,${this.props.height})`)
+    //   .call(xAxis);
+
+    // Add the Y Axis
+    const yAxis = d3.axisLeft(y).tickArguments(5);
+    const svg = d3.select(this.svg).transition();
+    svg.select('.yAxis').call(yAxis);
+  }
+
+  _renderLine = () => {
+    const { data } = this.props;
+    const { x, y } = this.state;
+
+    data.forEach((d) => {
+      d.date = new Date(d.timestamp);
+    });
 
     // Add the valueline path.
     const valueline = d3.line()
